e322211c8ec19064c53fb8130b0ea632f5b3e543
Jnt10 <janetchen1011@gmail.com>
2024-05-01 12:00:17 +0800

    week11

---
+++ week11/week11-4.py
@@ -0,0 +1,12 @@
+# LeetCode 2000. Reverse Prefix of word
+# 要把word裡, ch 字母(包含)之前的字,反過來
+# 這題的關鍵, 是先找到 ch 在 word 裡是第幾個位置。
+class Solution:
+    def reversePrefix(self, word: str, ch: str) -> str:
+        for i in range(len(word)): # 重點1:找到字母
+            if word[i]==ch: # 找到字母了,這時候i很重要
+                #重點2: 把 word[0]...word[i] 字反過來
+                return word[i::-1] + word[i+1:] # Python 的特殊範圍的寫法
+        # 如果都沒有提早結束,就是找不到。就原來的字迴轉
+        return word
+        
