f71cccb96282bbbf37af5eea3e80f6ba3f823bd2
Jnt10 <janetchen1011@gmail.com>
2024-06-19 11:18:29 +0800

    week18

---
+++ week18/week18-4.cpp
@@ -0,0 +1,16 @@
+// week18-1.cpp SOIT107_Advance_008_C
+#include <iostream>
+using namespace std;
+int prime(int n)
+{
+	for(int i=2; i<n; i++)
+	{
+		if(n%i==0) return 0;
+	}
+	return 1;
+}
+int main(){
+  int n;cin>>n;
+  cout<<"["<<prime(n)<<"]";
+  return 0;
+}
